5aa36a12826d44a2a8df317a00b2452186c7c3fe
Saatwik Mehta <saatwikmehta@gmail.com>
2022-01-02 18:27:00 +0530

    Made generic changes to the files

--- csv_db.py
+++ csv_db.py
@@ -1,14 +1,19 @@
+"""This module converts any given file data into db table that can be stored for later work"""
 import logging
 import os.path
 from mysql import connector
 import pandas as pd
 
-logging.basicConfig(filename='CRUD_operation.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%('
-                                                                              'message)s')
+logging.basicConfig(filename='CRUD_operation.log',
+                    level=logging.INFO,
+                    format='%(asctime)s: %(levelname)s:'
+                           ' %(filename)s->'
+                           ' %(funcName)s->'
+                           ' Line %(lineno)d-> %(message)s')
 
 PYTHON_TO_SQL_TYPE = {'string': 'TEXT',
                       'floating': 'FLOAT',
-                      'integer': 'INT',
+                      'integer': 'BIGINT',
                       'boolean': 'BOOLEAN',
                       'date': 'DATE',
                       'time': 'TIME'}
@@ -34,15 +39,19 @@ def csv_to_db(filename=None, primary_key_field: str = None, pk_field_dtype=None)
                 os.path.isfile(filename) and \
                 os.path.splitext(filename)[-1] == '.csv':
             table_name = os.path.splitext(filename)[0].lower()
+
             csv_dataframe = pd.read_csv(filename, keep_default_na=False)
+
             column_inf_dtype = [pd.api.types.infer_dtype(csv_dataframe[i], skipna=True)
                                 for i in csv_dataframe.columns]
             mysql_dtype_column = [PYTHON_TO_SQL_TYPE[i] for i in column_inf_dtype]
+            columns_in_df = [i.replace('.', '_') for i in list(csv_dataframe.columns)]
+            csv_dataframe.columns = columns_in_df
             column_with_dtype = [(i[0] + " " + i[1])
-                                 for i in zip(csv_dataframe[csv_dataframe.columns]
+                                 for i in zip(csv_dataframe[columns_in_df]
                                               , mysql_dtype_column)]
-            if primary_key_field is not None and primary_key_field != ''\
-                    and pk_field_dtype is not None and pk_field_dtype != ''\
+            if primary_key_field is not None and primary_key_field != '' \
+                    and pk_field_dtype is not None and pk_field_dtype != '' \
                     and primary_key_field in list(csv_dataframe.columns):
                 for i in range(len(column_with_dtype)):
                     if primary_key_field in column_with_dtype[i]:
@@ -57,11 +66,10 @@ def csv_to_db(filename=None, primary_key_field: str = None, pk_field_dtype=None)
                 cursor = conn.cursor()
                 cursor.execute("CREATE DATABASE IF NOT EXISTS fileupload")
                 cursor.execute("USE fileupload")
-                cursor.execute('DROP TABLE IF EXISTS {}'.format(table_name))
-                cursor.execute('CREATE TABLE IF NOT EXISTS {} ({})'.
-                               format(table_name, ','.join(column_with_dtype)))
+                cursor.execute(f'DROP TABLE IF EXISTS {table_name}')
+                cursor.execute(f'CREATE TABLE IF NOT EXISTS {table_name} ({",".join(column_with_dtype)})')
                 for i in csv_dataframe.values.tolist():
-                    cursor.execute('INSERT INTO {} VALUES {}'.format(table_name, tuple(i)))
+                    cursor.execute(f'INSERT INTO {table_name} VALUES {tuple(i)}')
                 conn.commit()
                 conn.close()
 
